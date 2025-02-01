bf649650d03f14220b247a43cd8536bf81774030
Adithya B R <59525997+adithya-b-r@users.noreply.github.com>
2025-02-01 14:14:10 +0530

    Update refresh_ip.ps1

--- refresh_ip.ps1
+++ refresh_ip.ps1
@@ -2,5 +2,5 @@
 # This script will release and renew the IP address of network adapters.
 
 Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | Disable-NetAdapter -Confirm:$false
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 3
 Get-NetAdapter | Where-Object {$_.Status -eq "Disabled"} | Enable-NetAdapter -Confirm:$false
